ec1c2655724ecae71c2caaadd44f249824247bf6
akinori <aki10.working@gmail.com>
2023-11-29 15:07:00 +0900

    first commit

--- app/api/hello/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import React from "react";
-export const GET = (req: NextApiRequest, res: NextApiResponse) => {
-    return new Response(JSON.stringify({ message: "Hello World" }))
-};
